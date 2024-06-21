8ea5c79468d6a3986ff6f8b3ee6b756e08870484
Bellal Hossain <59144648+bellalhossain-sosjb@users.noreply.github.com>
2024-06-21 15:44:00 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const header = document.querySelector("header");
+const menuBtn = document.querySelector("#menu-btn");
+const closeMenuBtn = document.querySelector("#close-menu-btn");
+
+menuBtn.addEventListener("click", () => {
+  header.classList.toggle("show-mobile-menu")
+})
+
+closeMenuBtn.addEventListener("click", () => {
+  menuBtn.click();
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
